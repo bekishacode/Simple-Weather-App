7acb8302ef8af6bfc411e02cd4116ecd9023aa60
Bereket Fikre <bekifikre19@gmail.com>
2023-10-07 21:49:51 +0300

    Updated

--- simpleWeatherApp/src/assets/index.js
+++ simpleWeatherApp/src/assets/index.js
@@ -1,6 +1,5 @@
 
-import dropbox from "./dropbox.png";
-import logo from "./logo.svg";
+
 import menu from "./menu.svg";
 import close from "./close.svg";
 import facebook from "./facebook.svg";
@@ -9,8 +8,6 @@ import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
 
 export {
-  dropbox,
-  logo,
   menu,
   close,
   facebook,
--- simpleWeatherApp/src/components/Navbar.jsx
+++ simpleWeatherApp/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { close, logo, menu } from "../assets";
+import { close, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
@@ -9,7 +9,6 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
       <h1 className="text-white">Weather App</h1>
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
